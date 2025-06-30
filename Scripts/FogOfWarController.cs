8fd75a649434a47e0382a7b498fa7f5e1274d13f
q-0q <jrwthrs@gmail.com>
2025-06-30 12:27:32 -0400

    cleanup files

--- Scripts/FogOfWarController.cs
+++ Scripts/FogOfWarController.cs
@@ -3,53 +3,26 @@ using UnityEngine.Serialization;
 
 public class FogOfWarController : MonoBehaviour
 {
-    public Material fogMaterial; // assign your FogOfWarRaymarch material here
-
-    public Transform player;     // player transform (world position used)
-    public float visionRadius = 5f;
-
     [Header("Occlusion texture world bounds")]
     public Vector3 worldMin = new Vector3(0, 0, 0);
     public Vector3 worldMax = new Vector3(10, 0, 10);
-    public int textureResolution = 256;
-    public ComputeShader computeShader;
     public RenderTexture occlusionTexture;
-    public RenderTexture outputTexture;
     
     public ComputeShader computeVolumeShader;
     public Material computeVolumeMaterial;
     public RenderTexture outputVolumeTexture;
-
-    private FogOfWarComputeShaderInterface _fogCompute;
+    
     private FogOfWarVolumeComputeShaderInterface _fogVolumeCompute;
     
     
 
     void Update()
     {
-        // FragmentShader();
         ComputeShader();
-    }
-
-    private void FragmentShader()
-    {
-        if (player is null || fogMaterial is null) return;
-
-        Vector3 pos = player.position;
-
-        // Pass player pos.x, pos.z + radius to shader (_PlayerPosRadius)
-        fogMaterial.SetVector("_PlayerPosRadius", new Vector4(pos.x, pos.z, visionRadius, 0));
-
-        // Pass world bounds used to map world space to UV coords in occlusion texture
-        fogMaterial.SetVector("_WorldMin", new Vector4(worldMin.x, worldMin.y, 0, 0));
-        fogMaterial.SetVector("_WorldMax", new Vector4(worldMax.x, worldMax.y, 0, 0));
     }
     
     void Start()
     {
-        // Setup output texture
-        // _fogCompute = new FogOfWarComputeShaderInterface();
-        // _fogCompute.Start(computeShader, occlusionTexture, outputTexture);
         _fogVolumeCompute = new FogOfWarVolumeComputeShaderInterface();
         _fogVolumeCompute.Start(computeVolumeShader, computeVolumeMaterial, occlusionTexture, outputVolumeTexture);
 
@@ -59,7 +32,6 @@ public class FogOfWarController : MonoBehaviour
     void ComputeShader()
     {
         HandleUnitVisibility();
-        // _fogCompute.Update(worldMin, worldMax);
         _fogVolumeCompute.Update(worldMin, worldMax);
     }
 
@@ -116,7 +88,6 @@ public class FogOfWarController : MonoBehaviour
     
     void LateUpdate()
     {
-        // _fogCompute.UpdateUnits(UnitController.Singleton.GetAlignedUnits());
         _fogVolumeCompute.UpdateUnits(UnitController.Singleton.GetAlignedUnits());
     }
 
